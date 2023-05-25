a0f257ff111756e6b0c008a024655492728b71dd
htazewell <39630448+htazewell@users.noreply.github.com>
2023-05-25 14:38:18 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='orchestration',
-    version='1.0.0',
+    version='1.0.7',
     description='This Python script provides functionality to schedule and execute scripts based on cron expressions. It allows executing both bash and Python scripts and provides options to add script parameters and skip conditions.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     author='Henry Tazewell',
     author_email='htazewell@gmail.com',
     url='https://github.com/htazwell/orchestration',
-    packages=['orchestration'],
+    packages=find_packages(),
     install_requires=[
         'croniter',
         'pytz',
